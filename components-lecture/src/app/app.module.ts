a9aab2adee0f2e2720e695866374e4bb0194c849
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-17 09:27:38 +0300

    Generate angular component

--- components-lecture/src/app/app.module.ts
+++ components-lecture/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [AppComponent, TestComponent],
--- components-lecture/src/app/test.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-test',
-  template: `<div>Test</div>`,
-})
-export class TestComponent {}
