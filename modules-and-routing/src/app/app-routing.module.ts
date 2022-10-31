4ad017d32b7b3002d2ea20f0cd74e21f60d82c26
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-31 10:32:55 +0200

    Add application paths to app routing

--- modules-and-routing/src/app/app-routing.module.ts
+++ modules-and-routing/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { UserListComponent } from './user/user-list/user-list.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/user-list',
+  },
+  {
+    path: 'user-list',
+    component: UserListComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
