82b794ca5cc27c9e57c23718f0b16b7539b7081a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-31 10:09:14 +0200

    Generate header component

--- modules-and-routing/src/app/app.module.ts
+++ modules-and-routing/src/app/app.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserListItemComponent } from './user-list-item/user-list-item.component';
 
 @NgModule({
-  declarations: [AppComponent, UserListComponent, UserListItemComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- modules-and-routing/src/app/user/user.module.ts
+++ modules-and-routing/src/app/user/user.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { UserListComponent } from './user-list/user-list.component';
+import { UserListItemComponent } from './user-list-item/user-list-item.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [UserListComponent, UserListItemComponent],
+  imports: [CommonModule],
 })
-export class UserModule { }
+export class UserModule {}
