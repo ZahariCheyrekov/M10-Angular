672b668c45c30f1220122db1e738f6aedd1ef75d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-31 10:28:25 +0200

    Generate about component

--- modules-and-routing/src/app/app.module.ts
+++ modules-and-routing/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { UserModule } from './user/user.module';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AboutComponent],
   imports: [BrowserModule, CoreModule, SharedModule, UserModule],
   providers: [],
   bootstrap: [AppComponent],
